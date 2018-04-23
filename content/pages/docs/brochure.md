a59baaa9ca9ff14af528971aeb3dce58ecfcca1e
Maurice Frank <maurice.frank@posteo.de>
2018-04-24 01:02:36 +0200

    Fixed logo size, removed DOM event trigger from main js and changed timeline abit

---
+++ content/pages/docs/brochure.md
@@ -0,0 +1,7 @@
+---
+title: "Die Produktbroschüre"
+slug: "brochure"
+date: 2018-03-27T20:26:42+02:00
+---
+
+{{< pdf src="docs/brochure.pdf" height="500px" >}}
--- static/main.js
+++ static/main.js
@@ -1,93 +1,91 @@
-document.addEventListener('DOMContentLoaded', function () {
-  // Toggles
-  var $burgers = getAll('.burger');
+// Toggles
+var $burgers = getAll('.burger');
 
-  if ($burgers.length > 0) {
-    $burgers.forEach(function ($el) {
-      $el.addEventListener('click', function () {
-        var target = $el.dataset.target;
-        var $target = document.getElementById(target);
-        $el.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
-      });
+if ($burgers.length > 0) {
+  $burgers.forEach(function ($el) {
+    $el.addEventListener('click', function () {
+      var target = $el.dataset.target;
+      var $target = document.getElementById(target);
+      $el.classList.toggle('is-active');
+      $target.classList.toggle('is-active');
     });
-  }
+  });
+}
 
-  // Delete
-  var $deletes = getAll('.delete');
-  if ($deletes.length > 0) {
-    $deletes.forEach(function ($el) {
-      $el.addEventListener('click', function () {
-        $el.parentElement.classList.toggle('is-hidden');
-      });
+// Delete
+var $deletes = getAll('.delete');
+if ($deletes.length > 0) {
+  $deletes.forEach(function ($el) {
+    $el.addEventListener('click', function () {
+      $el.parentElement.classList.toggle('is-hidden');
     });
-  }
+  });
+}
 
-  // Modals
-  var rootEl = document.documentElement;
-  var $modals = getAll('.modal');
-  var $modalButtons = getAll('.modal-button');
-  var $modalCloses = getAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button');
+// Modals
+var rootEl = document.documentElement;
+var $modals = getAll('.modal');
+var $modalButtons = getAll('.modal-button');
+var $modalCloses = getAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button');
 
-  if ($modalButtons.length > 0) {
-    $modalButtons.forEach(function ($el) {
-      $el.addEventListener('click', function () {
-        var target = $el.dataset.target;
-        openModal(target);
-      });
+if ($modalButtons.length > 0) {
+  $modalButtons.forEach(function ($el) {
+    $el.addEventListener('click', function () {
+      var target = $el.dataset.target;
+      openModal(target);
     });
-  }
+  });
+}
 
-  if ($modalCloses.length > 0) {
-    $modalCloses.forEach(function ($el) {
-      $el.addEventListener('click', function () {
-        closeModals();
-      });
+if ($modalCloses.length > 0) {
+  $modalCloses.forEach(function ($el) {
+    $el.addEventListener('click', function () {
+      closeModals();
     });
-  }
+  });
+}
 
-  function openModal(target) {
-    var $target = document.getElementById(target);
-    rootEl.classList.add('is-clipped');
-    $target.classList.add('is-active');
-    var img_nodes = getAll('#' + target + ' .modal-content img')
-    for(var i=0; i<img_nodes.length; i++) {
-      img_nodes[i].src = img_nodes[i].dataset.src;
-    }
+function openModal(target) {
+  var $target = document.getElementById(target);
+  rootEl.classList.add('is-clipped');
+  $target.classList.add('is-active');
+  var img_nodes = getAll('#' + target + ' .modal-content img')
+  for(var i=0; i<img_nodes.length; i++) {
+    img_nodes[i].src = img_nodes[i].dataset.src;
   }
+}
 
-  function closeModals() {
-    rootEl.classList.remove('is-clipped');
-    $modals.forEach(function ($el) {
-      $el.classList.remove('is-active');
-    });
-  }
+function closeModals() {
+  rootEl.classList.remove('is-clipped');
+  $modals.forEach(function ($el) {
+    $el.classList.remove('is-active');
+  });
+}
 
-  //Accordion
-  const MOUSE_EVENTS = ['click', 'touchstart'];
-  var accordions = document.querySelectorAll('.accordions');
-  [].forEach.call(accordions, function(accordion) {
-    var items = accordion.querySelectorAll('.accordion');
-    [].forEach.call(items, function(item) {
-      MOUSE_EVENTS.forEach((event) => {
-        item.querySelector('.toggle, [data-action="toggle"]').addEventListener(event, e => {
-          e.preventDefault();
-          if (!item.classList.contains('is-active')) {
-            let activeItem = accordion.querySelector('.accordion.is-active');
-            if (activeItem) {
-              activeItem.classList.remove('is-active');
-            }
-            item.classList.add('is-active');
-          } else {
-            item.classList.remove('is-active');
+//Accordion
+const MOUSE_EVENTS = ['click', 'touchstart'];
+var accordions = document.querySelectorAll('.accordions');
+[].forEach.call(accordions, function(accordion) {
+  var items = accordion.querySelectorAll('.accordion');
+  [].forEach.call(items, function(item) {
+    MOUSE_EVENTS.forEach((event) => {
+      item.querySelector('.toggle, [data-action="toggle"]').addEventListener(event, e => {
+        e.preventDefault();
+        if (!item.classList.contains('is-active')) {
+          let activeItem = accordion.querySelector('.accordion.is-active');
+          if (activeItem) {
+            activeItem.classList.remove('is-active');
           }
-        });
+          item.classList.add('is-active');
+        } else {
+          item.classList.remove('is-active');
+        }
       });
     });
   });
-
-  // Functions
-  function getAll(selector) {
-    return Array.prototype.slice.call(document.querySelectorAll(selector), 0);
-  }
 });
+
+// Functions
+function getAll(selector) {
+  return Array.prototype.slice.call(document.querySelectorAll(selector), 0);
+}
