7a97e910a26fc7ef3510a01f3a870af0b5030285
Maurice Frank <maurice.frank@posteo.de>
2018-08-27 13:11:21 +0200

    js better formatting

--- static/main.js
+++ static/main.js
@@ -1,19 +1,21 @@
-// Toggles
-var $burgers = getAll('.burger');
+// Burgers
+var burgers = document.getAll('.burger');
 
-if ($burgers.length > 0) {
-  $burgers.forEach(function ($el) {
-    $el.addEventListener('click', function () {
-      var target = $el.dataset.target;
-      var $target = document.getElementById(target);
-      $el.classList.toggle('is-active');
-      $target.classList.toggle('is-active');
+if (burgers.length > 0) setBurgerListeners();
+
+function setBurgerListeners() {
+  burgers.forEach(function (burger) {
+    burger.addEventListener('click', function () {
+      this.classList.toggle('is-active');
+      var target = document.getElementById(this.dataset.target);
+      target.classList.toggle('is-active');
     });
   });
 }
 
 // Delete
 var $deletes = getAll('.delete');
+
 if ($deletes.length > 0) {
   $deletes.forEach(function ($el) {
     $el.addEventListener('click', function () {
@@ -23,88 +25,95 @@ if ($deletes.length > 0) {
 }
 
 // Modals
-var rootEl = document.documentElement;
-var $modals = getAll('.modal');
-var $modalButtons = getAll('.modal-button');
-var $modalCloses = getAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button');
+var modals = getAll('.modal');
+var modalButtons = getAll('.modal-button');
+var modalClosers = getAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button');
 
-if ($modalButtons.length > 0) {
-  $modalButtons.forEach(function ($el) {
-    $el.addEventListener('click', function () {
-      var target = $el.dataset.target;
-      openModal(target);
+if (modalButtons.length > 0 && modalClosers.length > 0) {
+  setModalButtonListeners();
+  setModalCloserListeners();
+}
+
+function setModalButtonListeners() {
+  modalButtons.forEach(function (modalButton) {
+    modalButton.addEventListener('click', function () {
+      var modal = document.getElementById(this.dataset.target);
+      document.documentElement.classList.add('is-clipped');
+      modal.classList.add('is-active');
     });
   });
 }
 
-if ($modalCloses.length > 0) {
-  $modalCloses.forEach(function ($el) {
-    $el.addEventListener('click', function () {
-      closeModals();
+function setModalCloserListeners() {
+  modalClosers.forEach(function (modalCloser) {
+    modalCloser.addEventListener('click', function () {
+      document.documentElement.classList.remove('is-clipped');
+      modals.forEach(function (modal) {
+        modal.classList.remove('is-active');
+      });
     });
-  });
-}
-
-function openModal(target) {
-  var $target = document.getElementById(target);
-  rootEl.classList.add('is-clipped');
-  $target.classList.add('is-active');
-  var img_nodes = getAll('#' + target + ' .modal-content img');
-  for(var i=0; i<img_nodes.length; i++) {
-    img_nodes[i].src = img_nodes[i].dataset.src;
-  }
-}
-
-function closeModals() {
-  rootEl.classList.remove('is-clipped');
-  $modals.forEach(function ($el) {
-    $el.classList.remove('is-active');
   });
 }
 
 //Quote Grid
-var $quoteGridIcons = getAll('.quotegrid-icon');
-var $quoteGrids = getAll('.quotegrid-overlay');
+var quoteGridIcons = getAll('.quotegrid-icon');
+var quoteGrids = getAll('.quotegrid-overlay');
 
-$quoteGridIcons.forEach(function ($el) {
-  $el.addEventListener('click', function (e) {
-    if (e.target.tagName === 'FIGURE') {
-      $el.classList.add('is-active');
-      $el.querySelector('.quotegrid-overlay').classList.remove('is-hidden');
-    }
-  });
-});
+if (quoteGridIcons.length > 0 && quoteGrids.length > 0) {
+  setQuoteGridListeners();
+  setQuoteGridIconListeners();
+}
 
-$quoteGrids.forEach(function ($el) {
-  $el.addEventListener('click', function () {
-    $quoteGridIcons.forEach(function ($el) {
-      $el.classList.remove('is-active');
+function setQuoteGridIconListeners() {
+  quoteGridIcons.forEach(function (icon) {
+    icon.addEventListener('click', function (e) {
+      if (e.target.tagName === 'FIGURE') {
+        icon.classList.add('is-active');
+        icon.querySelector('.quotegrid-overlay').classList.remove('is-hidden');
+      }
     });
   });
-});
+}
+
+function setQuoteGridListeners() {
+  quoteGrids.forEach(function (quoteGrid) {
+    quoteGrid.addEventListener('click', function () {
+      quoteGridIcons.forEach(function (quoteGrid) {
+        quoteGrid.classList.remove('is-active');
+      });
+    });
+  });
+}
 
 //Accordion
 var MOUSE_EVENTS = ['click', 'touchstart'];
 var accordions = getAll('.accordions');
-accordions.forEach.call(function(accordion) {
-  var items = getAll('.accordions .accordion');
-  items.forEach.call(function(item) {
-    MOUSE_EVENTS.forEach(function (event) {
-      item.querySelector('.toggle, [data-action="toggle"]').addEventListener(event, e => {
-        e.preventDefault();
-        if (!item.classList.contains('is-active')) {
-          var activeItem = accordion.querySelector('.accordion.is-active');
-          if (activeItem) {
-            activeItem.classList.remove('is-active');
+
+if (accordions.length > 0) {
+  setAccordionListeners();
+}
+
+function setAccordionListeners() {
+  accordions.forEach.call(function (accordion) {
+    var items = getAll('.accordions .accordion');
+    items.forEach.call(function (item) {
+      MOUSE_EVENTS.forEach(function (event) {
+        item.querySelector('.toggle, [data-action="toggle"]').addEventListener(event, e => {
+          e.preventDefault();
+          if (!item.classList.contains('is-active')) {
+            var activeItem = accordion.querySelector('.accordion.is-active');
+            if (activeItem) {
+              activeItem.classList.remove('is-active');
+            }
+            item.classList.add('is-active');
+          } else {
+            item.classList.remove('is-active');
           }
-          item.classList.add('is-active');
-        } else {
-          item.classList.remove('is-active');
-        }
+        });
       });
     });
   });
-});
+}
 
 // Functions
 function getAll(selector) {
