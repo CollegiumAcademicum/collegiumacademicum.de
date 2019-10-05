fe9eabdc2b219b365e08e0419c49266f53e5389f
Maurice Frank <maurice.frank@posteo.de>
2019-10-05 17:40:53 +0200

    added absurl

--- build.sh
+++ build.sh
@@ -2,12 +2,12 @@
 PWD=$(pwd)
 hugo -d "${PWD}/upload"
 
-# Beautify everything
-find "${PWD}/upload" -type f -name "*.html" -exec node_modules/.bin/html-beautify -s 2 --no-preserve-newlines -r {} \;
-
 # Inline the Newsletters
 for dir in ${PWD}/upload/newsletter/*_*; do
     [ -d "${dir}" ] || continue
     echo "node_${dir}"
     ./node_modules/.bin/juice --web-resources-images=false "${dir}/index.html" "${dir}/index.html"
 done
+
+# Beautify everything
+# find "${PWD}/upload" -type f -name "*.html" -exec node_modules/.bin/html-beautify -s 2 --no-preserve-newlines -r {} \;
